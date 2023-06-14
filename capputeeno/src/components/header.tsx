e98b4bdb9980f1728c10b5e4c8b66612f5b54b26
Raysson <kustermendes10@gmail.com>
2023-06-14 20:12:08 -0300

    feat: creating hook localStorage for header

--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -1,17 +1,16 @@
-"use client"
-import { styled } from 'styled-components'
-import { Saira_Stencil_One } from 'next/font/google'
-import Link from 'next/link'
-import { PrimaryInput, PrimaryInputWSearchIcon } from './primary-input'
+"use client";
+import { styled } from "styled-components";
+import { Saira_Stencil_One } from "next/font/google";
+import Link from "next/link";
+import { PrimaryInput, PrimaryInputWSearchIcon } from "./primary-input";
+import { CartControl } from "./cart-control";
 
-interface HeaderProps {
-
-}
+interface HeaderProps {}
 
 const sairaStencil = Saira_Stencil_One({
-  weight: ['400'],
-  subsets: ['latin']
-})
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 const TagHeader = styled.header`
   display: flex;
@@ -19,24 +18,33 @@ const TagHeader = styled.header`
   justify-content: space-between;
 
   padding: 20px 160px;
-`
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+  }
+`;
 const Logo = styled.a`
-color: var(--logo-color);
-text-decoration: none;
+  color: var(--logo-color);
+  text-decoration: none;
 
-font-weight: 400;
-font-size: 40px;
-line-height: 150%;
-`
-
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 150%;
+`;
 
 export function Header(props: HeaderProps) {
   return (
     <TagHeader>
-      <Logo href='/' className={sairaStencil.className}>capputeeno</Logo>
+      <Logo href="/" className={sairaStencil.className}>
+        capputeeno
+      </Logo>
       <div>
-        <PrimaryInputWSearchIcon placeholder='O que voce está procurando ?'/>
+        <PrimaryInputWSearchIcon placeholder="O que voce está procurando ?" />
+        <CartControl />
       </div>
     </TagHeader>
-  )
-} 
+  );
+}
